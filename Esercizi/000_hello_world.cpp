99c36e357a42a53733205b4e89a849477e124e79
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2020-09-24 23:44:45 +0200

    Aggiunto 'Hello world'.

---
+++ Esercizi/000_hello_world.cpp
@@ -0,0 +1,18 @@
+/*
+    Università di Bologna
+    Corso di laurea in Informatica
+    00819 - Programmazione
+
+    Stefano Volpe #969766
+    24/09/2020
+
+    000_hello_world.cpp
+    Programma di esempio.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
